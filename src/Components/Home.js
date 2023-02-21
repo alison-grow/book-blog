f72d065cd53599307791b488d7f7efbc0c98a55e
alison-grow <agrowz98@gmail.com>
2023-02-20 21:13:36 -0800

    started editing

---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+    <div className="home-container">
+        hello
+        <div className="home-img">
+            {/* <img src={"./img/book-stack.jpg"} alt="stack of books" className="img" /> */}
+            hello
+        </div>
+    </div>
+}
